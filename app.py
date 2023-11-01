9f434ffd456a2b146e76937ff3ec667be0449ea4
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-11-01 11:03:20 -0700

    created results page

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/process", methods=["POST"])
+def process():
+    input_text = request.form.get("input_text")
+
+    # Process the input_text here and obtain label and score
+    # For this example, let's assume label and score are calculated.
+    label = "Positive"  # Replace with the actual label
+    score = 0.85  # Replace with the actual score
+
+    return render_template("result.html", label=label, score=score)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
