4b018ab83c8c85901aa04662863748a7fc18c7ec
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-11-01 11:36:06 -0700

    displays score

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template, request
+from transformers import pipeline
+import os
 
 app = Flask(__name__)
+
+sentiment_pipeline = pipeline("sentiment-analysis")
+text_generation_pipeline = pipeline("text-generation")
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+
+    return render_template("index.html", label="", score="")
 
 
 @app.route("/process", methods=["POST"])
 def process():
     input_text = request.form.get("input_text")
 
-    # Process the input_text here and obtain label and score
-    # For this example, let's assume label and score are calculated.
-    label = "Positive"  # Replace with the actual label
-    score = 0.85  # Replace with the actual score
+    sentiment_analysis = pipeline('sentiment-analysis')
+    sentiment_analysis(input_text)
 
-    return render_template("result.html", label=label, score=score)
+    label = sentiment_analysis(input_text)[0]['label']
+    score = sentiment_analysis(input_text)[0]['score']
+
+    generated_text = text_generation_pipeline(input_text, max_length=50, num_return_sequences=1)[0]['generated_text']
+
+    rephrased_suggestion = generated_text.strip()
+
+    return render_template("index.html", label=label, score=score, alternative_statement=rephrased_suggestion)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run()
